db6c946d00b1b9bc569590157c3916c771f688fc
ptanlam <phanlam20033002@gmail.com>
2022-01-10 08:19:03 +0700

    improved

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,10 @@
 
 docker compose up mssql postgres -d
 
-echo "Initializing mssql databases..."
+echo "Initializing postgres databases..."
 docker exec -it $(docker ps -f name="eshop-postgres*" -q --no-trunc) ./import-databases.sh
 
-echo "Initializing postgres databases..."
+echo "Initializing mssql databases..."
 docker exec $(docker ps -f name="eshop-mssql*" -q --no-trunc) ./import-databases.sh
 
 docker compose down
--- start.sh
+++ start.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-echo "Bootstrapping application..."
+echo "Starting application..."
 docker compose up mssql postgres rabbitmq redis mongo kong-gateway mailhog -d
 
-echo "It will take a couple of minutes..."
-sleep 30
-
-echo "Be patient!"
-sleep 20
-
-echo "Almost there"
-sleep 10
-
-echo "Starting services../"
-docker compose up -d
-echo "Complete bootstrapping application"
+echo "Starting services (it will take a couple of minutes)..."
+sleep 60 && docker compose up -d
+echo "Application is up and running."
